2b7e545804b13f1ff69b77297a0d9fdcce22fb75
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2021-11-23 14:12:34 -0800

    Added collect data function

--- feedback_rl/main.py
+++ feedback_rl/main.py
@@ -18,22 +18,34 @@ from feedback_rl.models import MLP
 def collect_data(num_steps):
     data = []
     env = gym.make("CartPoleSwingUp-v0")
-    env.reset()
-    done = False
     steps = 0
+    run = 0
 
     while steps < num_steps:
-        action = env.action_space.sample()
-        print(action)
-        obs, rew, done, info = env.step(action)
-        steps += 1
-        
+        env.reset()
+        done = False
+        data.append([])
+        while not done:
+            action = env.action_space.sample()
+            obs, rew, done, info = env.step(action)
+            steps += 1
+            data[run].append(obs)
+        data[run] = np.array(data[run])
+        run += 1
+
+    return data
         
 
 
-def main():
-    return
+def main(args):
+    start=  time.time()
+    data = collect_data(10_000)
+    print(f"Took {time.time() - start} seconds to collect data")
+    print(len(data))
+    [print(d.shape) for d in data]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     args = parser.parse_args()
+
+    main(args)
