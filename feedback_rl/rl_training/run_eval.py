03ad448e4707e9dc14d2289d8f560970416fb6a1
Ashwin Vangipuram <ashwinvangipuram@gmail.com>
2022-02-23 14:57:45 -0800

    added import

--- feedback_rl/rl_training/run_eval.py
+++ feedback_rl/rl_training/run_eval.py
@@ -12,36 +12,37 @@ import matplotlib.pyplot as plt
 
 from feedback_rl.envs import OfflineEtaEnv
 from feedback_rl.main import feedback_controller
+from feedback_rl.splines import ConstAccelSpline
 
 BASE_PATH = os.path.join(os.path.dirname(__file__), "../../runs")
 
 def evaluate(folder_name, model_name="best_model", render=False, iterations=10):
-	path = os.path.join(BASE_PATH, folder_name)
+    path = os.path.join(BASE_PATH, folder_name)
 
-	with open(os.path.join(path, "params.json"), 'r') as f:
-		params = DotMap(json.load(f))
+    with open(os.path.join(path, "params.json"), 'r') as f:
+        params = DotMap(json.load(f))
 
-	models_path = os.path.join(path, "models")
-	model = SAC.load(os.path.join(models_path, model_name))
+    models_path = os.path.join(path, "models")
+    model = SAC.load(os.path.join(models_path, model_name))
 
-	env_results = dict()
+    env_results = dict()
 
-	env = gym.make("CartPoleSwingUp-v0")
+    env = gym.make("CartPoleSwingUp-v0")
 
-	evaluations = np.load(os.path.join(path, "evaluations.npz"))
-	env_results["mean_reward"] = [evaluations["timesteps"], evaluations["results"]]
+    evaluations = np.load(os.path.join(path, "evaluations.npz"))
+    env_results["mean_reward"] = [evaluations["timesteps"], evaluations["results"]]
 
-	obs = env.reset()
-	done = False
-	total_reward = 0
+    obs = env.reset()
+    done = False
+    total_reward = 0
 
-	with open(os.path.join(BASE_PATH, params.eta_model, "args.json"), 'r') as f:
+    with open(os.path.join(BASE_PATH, params.eta_model, "args.json"), 'r') as f:
         eta_args = json.load(f)
 
-	while not done:
-		action, _states = model.predict(obs)
+    while not done:
+        action, _states = model.predict(obs)
 
-		spline = ConstAccelSpline(num_knots=2)
+        spline = ConstAccelSpline(num_knots=2)
         end_time = eta_args.prediction_horizon * env.params.deltat
         spline.random_spline([0, end_time], 1)
         spline.params = [action[0]]
@@ -50,26 +51,26 @@ def evaluate(folder_name, model_name="best_model", render=False, iterations=10):
 
         for i in range(eta_args.prediction_horizon):
 
-	        action = feedback_controller(env, i * env.params.deltat, spline, xi_initial)
+            action = feedback_controller(env, i * env.params.deltat, spline, xi_initial)
 
-			obs, reward, done, info = env.step(action)
-			total_reward += reward
-			if render:
-				env.render()
-				time.sleep(1/60)
-				
-	env.close()
+            obs, reward, done, info = env.step(action)
+            total_reward += reward
+            if render:
+                env.render()
+                time.sleep(1/60)
+                
+    env.close()
 
-	print("Episode reward:", total_reward)
+    print("Episode reward:", total_reward)
 
 
 if __name__=="__main__":
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--folder', '-f', type=str, default=None, help='Folder to evaluate.  Default: None')
-	parser.add_argument('--iterations', '-i', type=str, default=10, help='Number of iterations')
-	args = parser.parse_args()
-	if args.folder:
-		evaluate(args.folder, render=True)
-	else:
-		evaluate("02_16_2022_103854_", render=True, iterations=args.iterations)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folder', '-f', type=str, default=None, help='Folder to evaluate.  Default: None')
+    parser.add_argument('--iterations', '-i', type=str, default=10, help='Number of iterations')
+    args = parser.parse_args()
+    if args.folder:
+        evaluate(args.folder, render=True)
+    else:
+        evaluate("02_16_2022_103854_", render=True, iterations=args.iterations)
 
